59ba7d9698496342ee4739f1bf623e445186c38c
GerzsenyiRoland <103255961+GerzsenyiRoland@users.noreply.github.com>
2023-09-28 11:28:29 +0200

    OriginalTurkmiteTests added, exposed internal colors to be safer in the test.

--- TurkmiteTest/TurkmiteTest.cs
+++ TurkmiteTest/TurkmiteTest.cs
@@ -1,4 +1,5 @@
 using OpenCvSharp;
+using System.ComponentModel;
 using static TurkMite.Program;
 
 namespace TurkmiteTest
@@ -94,6 +95,43 @@ namespace TurkmiteTest
             var newColor = indexer[5, 5];
             Assert.AreEqual(turkmite.ReturnedColor, newColor);
             AssertTurkmiteState(6, 5, 1);
+        }
+
+        [TestClass]
+        public class OriginalTurkmiteTests
+        {
+            TestOriginalTurkmite turkmite = new TestOriginalTurkmite(new Mat(10, 10, MatType.CV_8UC3, new Scalar(0, 0, 0)));
+
+            [TestMethod]
+            public void BlackField_Correct()
+            {
+                var result = turkmite.GetNextColorAndUpdateDirection(turkmite.Black);
+                Assert.AreEqual(turkmite.White, result.newColor);
+                Assert.AreEqual(1, result.deltaDirection);
+            }
+
+            [TestMethod]
+            public void WhiteField_Correct()
+            {
+                var result = turkmite.GetNextColorAndUpdateDirection(turkmite.White);
+                Assert.AreEqual(turkmite.Black, result.newColor);
+                Assert.AreEqual(-1, result.deltaDirection);
+            }
+
+            private class TestOriginalTurkmite : OriginalTurkmite
+            {
+                public new (Vec3b newColor, int deltaDirection) GetNextColorAndUpdateDirection(Vec3b currentColor)
+                {
+                    return base.GetNextColorAndUpdateDirection(currentColor);
+                }
+
+                public Vec3b White { get { return White; } }
+                public Vec3b Black { get { return Black; } }
+
+                public TestOriginalTurkmite(Mat image) : base(image)
+                {
+                }
+            }
         }
     }
 }
