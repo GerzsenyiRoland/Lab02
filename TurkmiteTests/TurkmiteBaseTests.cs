6d411d559fb1f4aa39bef58bf51081ec654dc66a
GerzsenyiRoland <103255961+GerzsenyiRoland@users.noreply.github.com>
2023-09-28 10:30:22 +0200

    Test: Step calls GetNextColorAndUpdateDirection

--- TurkmiteTests/TurkmiteBaseTests.cs
+++ TurkmiteTests/TurkmiteBaseTests.cs
@@ -7,9 +7,25 @@ namespace TurkmiteTests
     public class TurkmiteBaseTests
     {
         [Fact]
-        public void Instantiation()
+        public void GetNextColorAndUpdateDirection_IsCalled()
         {
-            var t = new TurkMite.Program.OriginalTurkmite(new Mat(10, 10, MatType.CV_8UC3));
+            var t = new TestTurkmiteBase(new Mat(10,10,MatType.CV_8UC1));
+            t.Step();
+            Assert.True(t.GetNextColorAndUpdateDirectionInvoked);
+        }
+
+        class TestTurkmiteBase : TurkMite.Program.TurkmiteBase
+        {
+            public bool GetNextColorAndUpdateDirectionInvoked = false;
+
+            protected override (Vec3b newColor, int deltaDirection) GetNextColorAndUpdateDirection(Vec3b newColor)
+            {
+                GetNextColorAndUpdateDirectionInvoked=true;
+                return (new Vec3b(0, 0, 0), 0);
+            }
+            public TestTurkmiteBase(Mat img) : base(img) { }
+
+            public override int PerferredIterationCount => 0;
         }
     } 
 }
